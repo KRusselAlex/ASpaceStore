f50c4ae23bc59ae409359afeae7648b20ec6a976
KAlexRussel <kouawoualex1234@gmail.com>
2025-02-04 13:53:01 +0100

    about us page

--- my-anime-store/src/components/navbar/navbar.tsx
+++ my-anime-store/src/components/navbar/navbar.tsx
@@ -7,8 +7,8 @@ import "./style.css";
 
 const navItems = [
   { name: "Home", href: "/" },
-  { name: "Shop", href: "/about" },
-  { name: "Faqs", href: "/faqs" },
+  { name: "Shop", href: "/shop" },
+  { name: "About Us", href: "/about" },
   { name: "Contact Us", href: "/contact" },
   { name: "Login", href: "/login" },
 ];
